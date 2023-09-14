8f418089940b4925b3918142cd9fb1f7b7efeb4d
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-14 15:42:09 +0200

    test: Fix unit tests

--- test/cases.test.js
+++ test/cases.test.js
@@ -6,14 +6,15 @@ import Ajv from 'ajv';
 
 const ajv = new Ajv();
 const compiledSchemata = {};
-const tests = readdirSync('cases').filter(f => f.endsWith('.yml'));
+const casesPath = resolve('test', 'cases');
+const tests = readdirSync(casesPath).filter(f => f.endsWith('.yml'));
 
 describe("Unify", () => {
     tests.forEach(testCase => {
         it(`Validate ${testCase}`, async () => {
-            const test = YAML.parse(readFileSync(`cases/${testCase}`, 'utf-8'));
-            const schemaPromise = getSchema(resolve("../src", test.schema));
-            const content = JSON.parse(readFileSync(`cases/${testCase.replace(/yml$/, "content.json")}`, 'utf-8'));
+            const test = YAML.parse(readFileSync(resolve(casesPath, testCase), 'utf-8'));
+            const schemaPromise = getSchema(resolve("src", test.schema));
+            const content = JSON.parse(readFileSync(resolve(casesPath, testCase.replace(/yml$/, "content.json")), 'utf-8'));
 
             const unifiedSchema = await schemaPromise;
 
