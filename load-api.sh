51e2353d7ce84df92139654f8349f5d22cf50567
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-26 16:10:20 +0200

    fix: Load api script

--- load-api.sh
+++ load-api.sh
@@ -16,27 +16,29 @@ api_version=$(get_content 'version')
 api_files=$(get_content 'files' '*')
 
 function main() {
+  mkdir -p api
   # get the API version from the API dir
-  current_api_version=$(cat api/api-version.txt)
+  current_api_version=$(cat api/api-version.txt || echo "")
 
   # if the versions don't match, then we need to load the new API
   if [ "$api_version" != "$current_api_version" ]; then
     echo "Loading API version $api_version"
 
-    # TODO: download the API archive
-    # curl -Ls -o api/api.tar.gz "https://github.com/lenra-io/api/releases/download/v${api_version}/api.tar.gz"
+    # download the API archive
+    curl -Ls -o api/api.tar.gz "https://github.com/lenra-io/api/releases/download/v${api_version}/api.tar.gz"
     
     # save the new API version
     echo $api_version > api/api-version.txt
   fi
 
   cd api
+
   # load the new API files
   while IFS= read -r file; do
       if [ ! -f "$file" ]; then
-        echo "Extracting $1 from API version $api_version"
+        echo "Extracting $file from API version $api_version"
         # extract the file from the archive
-        tar -xzf api.tar.gz $1
+        tar -xzf api.tar.gz $file
       fi
   done <<< "$api_files"
 }
